5851f3aafa0fc7ed42a54e90836187e996abac86
Scott Carver <scarver@adobe.com>
2014-04-14 12:07:34 -0700

    Install libstdc++5 to possibly avoid a build error?

--- travis_before_install.sh
+++ travis_before_install.sh
@@ -3,7 +3,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	sudo apt-add-repository -y ppa:ubuntu-sdk-team/ppa
 	sudo apt-get update
 	#sudo apt-get install qt5-default qt5-qmake qttools5-dev qttools5-dev-tools qtdeclarative5-dev libqt5webkit5-dev qtchooser
-	sudo apt-get install build-essential libqt4-dev libqtwebkit-dev libjack-dev libsndfile1-dev libasound2-dev libavahi-client-dev libicu-dev libreadline6-dev libfftw3-dev libxt-dev pkg-config cmake subversion
+	sudo apt-get install build-essential libqt4-dev libqtwebkit-dev libjack-dev libsndfile1-dev libasound2-dev libavahi-client-dev libicu-dev libreadline6-dev libfftw3-dev libxt-dev pkg-config cmake subversion libstdc++5
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	brew update
